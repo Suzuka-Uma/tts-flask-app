a8b0a64a4508f7675f13f02286fa118b25a96ca5
Trần Hữu Đạt <522h0081@student.tdtu.edu.vn>
2026-02-21 20:24:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def serve_audio(filename):
     return send_from_directory(AUDIO_DIR, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
